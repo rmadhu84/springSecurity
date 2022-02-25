686cf9187b1201c16912cbd98685c490af8d28d5
rmadhu84 <r.madhu.84@gmail.com>
2022-02-25 16:01:36 -0500

    Final check in

--- spring-security-jdbc/src/main/java/com/madhu/springsecurityjdbc/controller/HomeController.java
+++ spring-security-jdbc/src/main/java/com/madhu/springsecurityjdbc/controller/HomeController.java
@@ -4,23 +4,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HomeController {
 
     @GetMapping(value = "/")
-    public ResponseEntity<Object> home(){
+    public ResponseEntity<Object> home() {
         return ResponseEntity.status(HttpStatus.OK).body("<h1>Welcome</h1>");
     }
 
     @GetMapping(value = "/user")
-    public ResponseEntity<Object> user(){
+    public ResponseEntity<Object> user() {
         return ResponseEntity.status(HttpStatus.OK).body("<h1>Welcome, User</h1>");
     }
 
     @GetMapping(value = "/admin")
-    public ResponseEntity<Object> admin(){
+    public ResponseEntity<Object> admin() {
         return ResponseEntity.status(HttpStatus.OK).body("<h1>Welcome, Admin</h1>");
     }
-    
+
+    @GetMapping(value = "/greet")
+    public ResponseEntity<Object> greet(@RequestParam String param) {
+        return ResponseEntity.status(HttpStatus.OK).body(String.format("Hello, %s", param));
+    }
+
 }
